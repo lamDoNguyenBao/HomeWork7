60442780a90560db61db281771894f3b17b45e87
Đỗ Nguyễn Bảo Lâm <lamdnb1234@gmail.com>
2018-08-01 17:16:29 +0700

    update recycleview

--- app/src/main/java/com/example/lamdonguyenbao/homework7/activity/MainActivity.java
+++ app/src/main/java/com/example/lamdonguyenbao/homework7/activity/MainActivity.java
@@ -5,6 +5,9 @@ import android.database.Cursor;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -13,8 +16,11 @@ import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.lamdonguyenbao.homework7.R;
@@ -24,18 +30,19 @@ import com.example.lamdonguyenbao.homework7.model.Contacts;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ContactsAdapter.OnItemLongClickListener {
     private static final String COLUMNS_NAME = "name";
     private static final String COLUMNS_NUMBER = "number";
 
-    ListView lv_contact;
+    RecyclerView lv_contact;
+    Switch sw_view;
     ArrayList<Contacts> listContact;
     ContactsAdapter contactAdapter;
     Database database;
-    boolean checked = false;
-    MenuItem itemRecycle;
-    String sql="";
+    MenuItem itemDelete,itemAdd;
+    String sql = "";
     Toolbar toolbar;
+    LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,83 +53,100 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        lv_contact = (ListView) findViewById(R.id.lv_contact);
+        listContact = new ArrayList<>();
+        lv_contact = (RecyclerView) findViewById(R.id.lv_contact);
+        sw_view = (Switch) findViewById(R.id.sw_view);
         database = new Database(this, "contact.sqlite", null, 1);
+
+        lv_contact.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        lv_contact.setLayoutManager(layoutManager);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        contactAdapter = new ContactsAdapter(MainActivity.this,
+                listContact);
+        contactAdapter.setOnItemLongClickListener(this);
+        lv_contact.setAdapter(contactAdapter);
+        
         createTable();
         getDB();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        itemRecycle = menu.findItem(R.id.btn_delete);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        itemDelete = menu.findItem(R.id.btn_delete);
+        itemAdd = menu.findItem(R.id.btn_add);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.btn_add:
                 getDialog();
                 break;
             case R.id.btn_delete:
-                for(Contacts contact : listContact){
-                    if(contact.isChecked()){
-                        Log.d("check", "onClick: "+contact.getName());
-                        sql ="delete from Contacts where "+COLUMNS_NAME+" ='"+contact.getName()+"' and "+COLUMNS_NUMBER+" ='"+contact.getNumber()+"'";
+                for (Contacts contact : listContact) {
+                    if (contact.isChecked()) {
+                        sql = "delete from Contacts where " + COLUMNS_NAME + " ='" + contact.getName() + "' and " + COLUMNS_NUMBER + " ='" + contact.getNumber() + "'";
                         new SetDatabase().execute(sql);
                     }
                 }
-                if(listContact.size() ==0){
-                    hideButtonDelete();
-                }
+                hideButtonDelete();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
     /*
-    * get data from sqlite
-    */
-    private void getDB(){
-        listContact = new ArrayList<>();
+     * get data from sqlite
+     */
+    private void getDB() {
+        listContact.clear();
         sql = "select * from Contacts";
         Cursor dataContacts = database.getData(sql);
-        while (dataContacts.moveToNext()){
+        while (dataContacts.moveToNext()) {
             String name = dataContacts.getString(dataContacts.getColumnIndex(COLUMNS_NAME));
             String number = dataContacts.getString(dataContacts.getColumnIndex(COLUMNS_NUMBER));
-            Contacts contacts = new Contacts(name,number);
+            Contacts contacts = new Contacts(name, number);
             listContact.add(contacts);
         }
-        contactAdapter = new ContactsAdapter(MainActivity.this,
-                R.layout.inline_contact,
-                listContact,checked);
-        lv_contact.setAdapter(contactAdapter);
+
+        contactAdapter.notifyDataSetChanged();
     }
 
     /**
      * create table Contacts in sqlite
-     * */
-    private void createTable(){
-        sql ="CREATE TABLE IF NOT EXISTS Contacts(Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMNS_NAME+" VARCHAR(200),"+COLUMNS_NUMBER+" VARCHAR(200) )";
+     */
+    private void createTable() {
+        sql = "CREATE TABLE IF NOT EXISTS Contacts(Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMNS_NAME + " VARCHAR(200)," + COLUMNS_NUMBER + " VARCHAR(200) )";
         new SetDatabase().execute(sql);
 //        database.queryData(sql);
     }
 
-    /*
-    * excute query in async task
-    * */
+    @Override
+    public void onItemClick(int position) {
+        itemDelete.setVisible(true);
+        itemAdd.setEnabled(false);
+        listContact.get(position).setChecked(true);
+        contactAdapter.notifyDataSetChanged();
+    }
 
-    private class SetDatabase extends AsyncTask<String, String, String>{
+    /*
+     * excute query in async task
+     * */
+
+    private class SetDatabase extends AsyncTask<String, String, String> {
         @Override
         protected String doInBackground(String... strings) {
             try {
                 database.queryData(strings[0]);
                 return "true";
-            }catch (Exception e){
+            } catch (Exception e) {
                 return "false";
             }
         }
@@ -130,28 +154,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            if(s.equals("true")){
+            if (s.equals("true")) {
                 getDB();
-            }else{
-                Toast.makeText(MainActivity.this,"Error when excute sql",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Error when excute sql", Toast.LENGTH_SHORT).show();
             }
-
         }
     }
 
     private void process() {
-        lv_contact.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        sw_view.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                checked = true;
-//                btn_delete.setVisibility(View.VISIBLE);
-                itemRecycle.setVisible(true);
-                contactAdapter = new ContactsAdapter(MainActivity.this,
-                        R.layout.inline_contact,
-                        listContact,checked);
-                lv_contact.setAdapter(contactAdapter);
-                listContact.get(position).setChecked(true);
-                return false;
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    layoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+                } else {
+                    layoutManager = new GridLayoutManager(MainActivity.this, 2);
+                }
+                lv_contact.setLayoutManager(layoutManager);
             }
         });
     }
@@ -160,52 +180,47 @@ public class MainActivity extends AppCompatActivity {
      *hide button delete
      */
 
-    private void hideButtonDelete(){
-        checked = false;
-//        btn_delete.setVisibility(View.INVISIBLE);
-        itemRecycle.setVisible(false);
-        for(Contacts item:listContact){
+    private void hideButtonDelete() {
+        itemDelete.setVisible(false);
+        itemAdd.setEnabled(true);
+        for (Contacts item : listContact) {
             item.setChecked(false);
         }
-        contactAdapter = new ContactsAdapter(MainActivity.this,
-                R.layout.inline_contact,
-                listContact,checked);
-        lv_contact.setAdapter(contactAdapter);
+        contactAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onBackPressed() {
-        if(checked){
+        if (itemDelete.isVisible()) {
             hideButtonDelete();
             return;
-        }else{
+        } else {
             super.onBackPressed();
         }
     }
 
     /*
-    *show dialog and set event for button in dialog
-    */
+     *show dialog and set event for button in dialog
+     */
     private void getDialog() {
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.dialog_add_custom);
         final EditText et_name = (EditText) dialog.findViewById(R.id.et_name);
         final EditText et_number = (EditText) dialog.findViewById(R.id.et_number);
-        Button btn_cancel= (Button) dialog.findViewById(R.id.btn_cancel);
+        Button btn_cancel = (Button) dialog.findViewById(R.id.btn_cancel);
         Button btn_save = (Button) dialog.findViewById(R.id.btn_save);
-
 
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name = et_name.getText().toString();
                 String number = et_number.getText().toString();
-                if("".equals(name.trim()) ||"".equals(number.trim())){
-                    Toast.makeText(MainActivity.this,"name and number can not be empty",Toast.LENGTH_SHORT).show();
+                if ("".equals(name.trim()) || "".equals(number.trim())) {
+                    Toast.makeText(MainActivity.this, "name and number can not be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                sql = "INSERT INTO Contacts VALUES(null,'"+name+"','"+number+"')";
+                sql = "INSERT INTO Contacts VALUES(null,'" + name + "','" + number + "')";
                 new SetDatabase().execute(sql);
                 dialog.dismiss();
             }
--- app/src/main/java/com/example/lamdonguyenbao/homework7/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/lamdonguyenbao/homework7/adapter/ContactsAdapter.java
@@ -1,10 +1,12 @@
 package com.example.lamdonguyenbao.homework7.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -14,67 +16,40 @@ import com.example.lamdonguyenbao.homework7.model.Contacts;
 
 import java.util.List;
 
-public class ContactsAdapter extends BaseAdapter {
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
 
     private Context context;
-    private int layout;
     private List<Contacts> listContacts;
-    private boolean checked;
+    private OnItemLongClickListener mListener;
 
-
-    public ContactsAdapter(Context context, int layout, List<Contacts> listContacts,boolean checked) {
+    public ContactsAdapter(Context context, List<Contacts> listContacts) {
         this.context = context;
-        this.layout = layout;
         this.listContacts = listContacts;
-        this.checked = checked;
     }
 
+    @NonNull
     @Override
-    public int getCount() {
-        if(listContacts == null){
-            return 0;
-        }else{
-            return listContacts.size();
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView= inflater.inflate(R.layout.inline_contact,parent,false);
+        return new ViewHolder(itemView);
+    }
+
+    private boolean isChecked(){
+        for (Contacts contact: listContacts) {
+                if(contact.isChecked()){
+                    return true;
+                }
         }
+        return false;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    private class Holder{
-        TextView tv_name,tv_number;
-        CheckBox cb_check;
-    }
-
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        Holder holder;
-        if(convertView == null){
-            holder = new Holder();
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            convertView = inflater.inflate(layout,null);
-
-            holder.tv_name = (TextView) convertView.findViewById(R.id.tv_name);
-            holder.tv_number = (TextView) convertView.findViewById(R.id.tv_number);
-            holder.cb_check = (CheckBox) convertView.findViewById(R.id.cb_check);
-
-            convertView.setTag(holder);
-        }else{
-            holder = (Holder) convertView.getTag();
-        }
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Contacts contact = listContacts.get(position);
         holder.tv_name.setText(contact.getName());
         holder.tv_number.setText(contact.getNumber());
-        if(checked){
+        if(isChecked()){
             holder.cb_check.setVisibility(View.VISIBLE);
             if(listContacts.get(position).isChecked()){
                 holder.cb_check.setChecked(true);
@@ -84,13 +59,49 @@ public class ContactsAdapter extends BaseAdapter {
         }else{
             holder.cb_check.setVisibility(View.INVISIBLE);
         }
-
         holder.cb_check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 listContacts.get(position).setChecked(isChecked);
             }
         });
-        return convertView;
+    }
+
+    @Override
+    public int getItemCount() {
+        if(listContacts == null){
+            return 0;
+        }else{
+            return listContacts.size();
+        }
+    }
+
+    public void setOnItemLongClickListener(@NonNull OnItemLongClickListener listener){
+        this.mListener = listener;
+    }
+
+    public interface OnItemLongClickListener {
+        void onItemClick(int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
+        TextView tv_name,tv_number;
+        CheckBox cb_check;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            tv_number = (TextView) itemView.findViewById(R.id.tv_number);
+            cb_check = (CheckBox) itemView.findViewById(R.id.cb_check);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if(mListener != null){
+                mListener.onItemClick(getAdapterPosition());
+                Log.d("check", "onLongClick: "+getAdapterPosition());
+            }
+            return false;
+        }
     }
 }
